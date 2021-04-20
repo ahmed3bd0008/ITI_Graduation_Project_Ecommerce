6352888340f76006e597e62dd4ef9d2868a4c8a6
doaa rabie <doaa.rabie22@gmail.com>
2021-04-20 13:18:33 +0200

    commit_doaa

---
+++ MahaleSystem/Repository/Interface/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using MahaleSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MahaleSystem.Repository.Interface
+{
+    interface IProductRepository: IGenaricRepository<Product>
+    {
+    }
+}
